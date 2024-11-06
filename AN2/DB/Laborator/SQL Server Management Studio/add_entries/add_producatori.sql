90b99d91a4fb7a13a70268e7e53bbdd84d5848d4
unknown <mmike73>
2024-11-06 13:58:43 -0800

    Remove deleted files

--- AN2/DB/Laborator/SQL Server Management Studio/add_entries/add_producatori.sql
+++
@@ -1,8 +0,0 @@
-USE [pc_parts_manager_2];
-GO
-
-INSERT INTO producatori (denumire) VALUES 
-('Intel'), ('AMD'), ('ASUS'), ('Gigabyte'), ('Corsair'),
-('Seagate'), ('NVIDIA'), ('MSI'), ('Samsung'), ('Kingston'),
-('Western Digital'), ('Cooler Master'), ('EVGA'), ('Crucial'), ('Asrock');
-GO
